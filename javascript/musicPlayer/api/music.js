a701a1fb8ad122f77510fef1243d4fd7bd61d13d
sungilRyuu <x7nxc3@gmail.com>
2022-04-07 17:48:11 +0900

    0407

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,63 @@
+
+        let allMusic = [
+            {
+                name: "Whiskey Blues",
+                artist: "Sound For you",
+                img: "bg1",
+                audio: "SoundForYou_-_Whiskey_Blues"
+            },
+            {
+                name: "Insane Love",
+                artist: "AudioMagma",
+                img: "bg2",
+                audio: "AudioMagma_-_Insane_Love"
+            },
+            {
+                name: "Sea Of The Lost And The Pharohs",
+                artist: "Avi Rosenfeld",
+                img: "bg3",
+                audio: "Avi_Rosenfeld_-_10_-_SeaOfTheLostAndThePharohs"
+            },
+            {
+                name: "BJ Wilbanks",
+                artist: "Change Your Mind",
+                img: "bg4",
+                audio: "BJ_Wilbanks_-_Change_Your_Mind"
+            },
+            {
+                name: "Alone",
+                artist: "color out",
+                img: "bg5",
+                audio: "Color_Out_-_Alone"
+            },
+            {
+                name: "Electro Sport EDM",
+                artist: "NEONMusic",
+                img: "bg6",
+                audio: "NEONMusic_-_Electro_Sport_EDM"
+            },
+            {
+                name: "On returning",
+                artist: "Amber eyes Instrumental",
+                img: "bg7",
+                audio: "On_returning_-_Amber_eyes__Instrumental_"
+            },
+            {
+                name: "On returning",
+                artist: "Farewell song Instrumental",
+                img: "bg8",
+                audio: "On_returning_-_Farewell_song__Instrumental_"
+            },
+            {
+                name: "Waves Of Energy",
+                artist: "Papasound",
+                img: "bg9",
+                audio: "Papasound_-_Waves_Of_Energy"
+            },
+            {
+                name: "Sound For You",
+                artist: "Whiskey Blues",
+                img: "bg10",
+                audio: "SoundForYou_-_Whiskey_Blues"
+            },
+        ]
