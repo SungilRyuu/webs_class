5805804343b0e9da3e8059863ecf56ba76db5e11
sungilRyuu <x7nxc3@gmail.com>
2022-05-19 18:11:10 +0900

    0519

--- portfolio/0517 다시/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-var s = skrollr.init();
-
-function scroll(){
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.screenY;
-    document.querySelector(".scrollTop").innerText = Math.round(scrollTop);    
-
-    requestAnimationFrame(scroll);
-}
-scroll();
