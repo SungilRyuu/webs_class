48a6d064851775a89e5e68878bb9065de97964aa
sungilRyuu <x7nxc3@gmail.com>
2022-05-24 09:24:04 +0900

    0524

--- portfolio/하는 중/assets/js/index.js
+++ portfolio/하는 중/assets/js/index.js
@@ -7,3 +7,16 @@ function scroll(){
     requestAnimationFrame(scroll);
 }
 scroll();
+
+// 사이즈를 변경하면 재로드
+window.onresize = () => {
+    location.reload();
+}
+
+// intro 횡단보도
+const loaders = document.querySelector(".loader");
+window.onload = function(){
+    setTimeout(() => {
+        loaders.classList.remove('loader__active');
+    }, 700);
+};
