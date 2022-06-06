37dc5e1edf9c0ddf44af83c8aac417a1ff4371c1
sungilRyuu <x7nxc3@gmail.com>
2022-06-07 00:19:03 +0900

    0530

--- portfolio/work/assets/js/index.js
+++ portfolio/work/assets/js/index.js
@@ -1,3 +1,63 @@
+// 시계
+    var lastHr,
+    lastMin;
+function leTime() {
+    this.fullDate = new Date();
+    this.secs = this.fullDate.getSeconds();
+    this.mins = this.fullDate.getMinutes();
+    this.hrs = this.fullDate.getHours() % 12;
+    return this;
+}
+function leClock() {
+    var sec = document.getElementById("seconds"),
+        min = document.getElementById("minutes"),
+        hr = document.getElementById("hours");
+    if (leTime().secs == 0) {
+        TweenMax.to(sec, 0.35, {
+            rotation: 360,
+            ease: Elastic.easeOut.config(1, 0.4),
+            clearProps: "all"
+        });
+    } else {
+        TweenMax.to(sec, 0.35, {
+            rotation: leTime().secs * 6,
+            ease: Elastic.easeOut.config(1, 0.4)
+        });
+    }
+    if (leTime().mins !== lastMin) {
+        if (leTime().mins == 0) {
+            TweenMax.to(min, 0.35, {
+                rotation: 360,
+                ease: Elastic.easeOut.config(1, 0.4),
+                clearProps: "all"
+            });
+        } else {
+            TweenMax.to(min, 0.35, {
+                rotation: leTime().mins * 6,
+                ease: Elastic.easeOut.config(1, 0.4)
+            });
+        }
+    }
+    if (leTime().hrs !== lastHr) {
+        if (leTime().hrs == 0) {
+            TweenMax.to(hr, 0.35, {
+                rotation: 360,
+                ease: Elastic.easeOut.config(1, 0.4),
+                clearProps: "all"
+            });
+        } else {
+            TweenMax.to(hr, 0.35, {
+                rotation: leTime().hrs * 30,
+                ease: Elastic.easeOut.config(1, 0.4)
+            });
+        }
+    }
+    lastHr = leTime().hrs;
+    lastMin = leTime().mins;
+    setTimeout("leClock()", 1000);
+}
+window.onload = leClock();
+
 // 메일 보내기
 const prevMail = document.querySelector(".sec10__body p");
 const formMail = document.querySelector(".mail");
@@ -19,27 +79,7 @@ document.querySelector("#section10").addEventListener("mousemove", (e) => {
   gsap.to(".games", {duration: 0.3, x: centerPage/10});
 })
 
-// section1 액자
-  document.querySelector(".sec1__btn").addEventListener("click", () => {reload()});
-  let reloadTime = true;
 
-  function reload(){
-    setTimeout(() => {
-      reloadTime = false;
-    }, 3000);
-
-    if(reloadTime == false){
-      document.querySelectorAll(".reveal").forEach(reveal => {
-        reveal.classList.add("revealing");
-        setTimeout(() => {
-          location.reload();
-        }, 1000);
-      })
-      reloadTime = true;
-    }
-  }
-  reload();
-</script>
 
 // 고래
     const canvas = document.getElementById("canvas");
